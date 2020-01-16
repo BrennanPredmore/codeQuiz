06d15206fe8fe2a59ed6840ae11561e39173e447
Your Brennan Predmore <brennnann@gmail.com>
2020-01-16 17:56:00 -0600

    remove useless and unused code

--- script.js
+++ script.js
@@ -4,33 +4,10 @@ var ul = document.querySelector('#choices')
 var currentQuestionIndex = 0
 var timer;
 
-// var choiceOneElem = document.querySelector("#choiceOne");
-// var choiceTwoElem = document.querySelector("#choiceTwo");
-// var choiceThreeElem = document.querySelector("#choiceThree");
-// var choiceFourElem = document.querySelector("#choiceFour");
-
 var startButton = document.querySelector('#startButton')
 .addEventListener('click', startButtonClicked);
 
 function startButtonClicked(e){
-    // document.querySelector('#questionTitle').textContent = 'Commonly used data types do not include:';
-    // document.querySelector('#paragraph').innerHTML = '';
-
-    // var choiceOneBtn = document.createElement("button");
-    // choiceOneBtn.textContent = '1. Strings';
-    // choiceOneElem.append(choiceOneBtn);
-
-    // var choiceTwoBtn = document.createElement("button");
-    // choiceTwoBtn.textContent = '2. booleans';
-    // choiceTwoElem.append(choiceTwoBtn);
-
-    // var choiceThreeBtn = document.createElement("button");
-    // choiceThreeBtn.textContent = '3. alerts';
-    // choiceThreeElem.append(choiceThreeBtn);
-
-    // var choiceFourBtn = document.createElement("button");
-    // choiceFourBtn.textContent = '4. numbers';
-    // choiceFourElem.append(choiceFourBtn);
     var fiveMinutes = 15 * 5,
     display = document.querySelector('#time');
 startTimer(fiveMinutes, display);
@@ -70,24 +47,6 @@ function displayQuestion(e){
 
     
 }}
-
-
-// var submitButton = document.querySelector('#startButton')
-// .addEventListener('click', submitAnswer);
-
-        // Use delegator strategy, have event listener on predefined element main
-        // wait for clicks on main element
-        // ul.addEventListener("click", function(event) {
-
-        //     var targetedClick = event.target;
-            
-        //     // Only target li tags to proceed to next classroom
-        //     if(targetedClick.matches("button")) {
-
-        //         console.log(targetedClick.textContent);
-        //         answerButtonClicked()
-        //     }
-        // });
 
 
         // SET TIMER!
