d45471d0da013211db61973e5c676514d89a9329
Your Brennan Predmore <brennnann@gmail.com>
2020-01-16 13:51:26 -0600

    set timer init to 1:15

--- script.js
+++ script.js
@@ -58,3 +58,30 @@ function buttonClicked(e){
         });
 
 
+        // SET TIMER!
+        var time = document.getElementById('time');
+
+        time.addEventListener('click', buttonClicked);
+
+        function startTimer(duration, display) {
+            var timer = duration, minutes, seconds;
+            setInterval(function () {
+                minutes = parseInt(timer / 60, 10);
+                seconds = parseInt(timer % 60, 10);
+        
+                minutes = minutes < 10 ? "0" + minutes : minutes;
+                seconds = seconds < 10 ? "0" + seconds : seconds;
+        
+                display.textContent = minutes + ":" + seconds;
+        
+                if (--timer < 0) {
+                    timer = duration;
+                }
+            }, 1000);
+        }
+        
+        window.onload = function () {
+            var fiveMinutes = 15 * 5,
+                display = document.querySelector('#time');
+            startTimer(fiveMinutes, display);
+        };
