efaf12edcfa9b8279a203f3bc55c1f5fc39de0fc
Your Brennan Predmore <brennnann@gmail.com>
2020-03-24 17:15:16 -0500

    view highscores init

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ var paragraphSection = document.querySelector('#paragraph');
 var ul = document.querySelector('#choices')
 var currentQuestionIndex = 0
 var timer;
+var timerInterval;
+var display;
 
 var startButton = document.querySelector('#startButton')
 .addEventListener('click', startButtonClicked);
@@ -29,14 +31,30 @@ currentQuestionIndex++
 displayQuestion() 
 }
 
+function endGame(){
+    clearInterval(timerInterval);
+        console.log("game over");
+        //display results DIV 
+        document.querySelector('#resultsDIV').style.display = "block"
+        // Hide Choices DIV
+        document.querySelector('.container').style.display = "none"
+        // add score final
+        document.querySelector('#score').textContent = timer;
+        document.querySelector('#time').textContent = timer;
+}
+
 function displayQuestion(e){
     var currentQuestion = questions[currentQuestionIndex];
+    if( !currentQuestion){
+      return endGame()
+       
+    }
     document.querySelector('#questionTitle').textContent = currentQuestion.title;
     document.querySelector('#paragraph').innerHTML = '';
     ul.innerHTML = '';
   
     for (var i = 0; i < currentQuestion.choices.length;i++){
-        if(currentQuestion){
+        
         var li = document.createElement('li');
           var choiceButton = document.createElement("button");
           choiceButton.setAttribute('value', currentQuestion.choices[i])
@@ -44,14 +62,7 @@ function displayQuestion(e){
  choiceButton.onclick = answerButtonClicked
  choiceButton.classList.add('answerChoice')
  li.append(choiceButton)
- ul.append(li);}
-        else{
-            //stop timer 
-            //manipulate dom to END PAGE
-            console.log('no more questions');
-        } 
-        
-
+ ul.append(li);
     
 }}
 
@@ -65,7 +76,7 @@ function displayQuestion(e){
              timer = duration;
              var minutes; 
              var seconds;
-            setInterval(function () {
+            timerInterval = setInterval(function () {
                 minutes = parseInt(timer / 60, 10);
                 seconds = parseInt(timer % 60, 10);
         
@@ -85,7 +96,31 @@ function displayQuestion(e){
         };
 
         //Local storage
-        // Store
-localStorage.setItem("initials", timer);
+       // Store
+       document.querySelector('#submit').addEventListener('submit', function (event){
+           event.preventDefault()
+           var initials = document.querySelector('#initials').value
+           localStorage.setItem(initials, timer);
+       } )
+
 // Retrieve
-document.getElementById("result").innerHTML = localStorage.getItem('intitials');
+// document.getElementById("result").innerHTML = localStorage.getItem('intitials');
+document.querySelector("#resultsDIV").style.display = "none";
+document.querySelector("#scoresDIV").style.display = "none";
+
+
+document.querySelector("#viewScores").addEventListener('click', displayScores)
+function displayScores(){
+    document.querySelector("#scoresDIV").style.display = "block"
+    Object.keys(localStorage).forEach(function(key){
+        console.log(localStorage.getItem(key)); 
+        
+        var li = document.createElement('li');
+    li.textContent = key + '-' + localStorage.getItem(key)
+    document.querySelector('#scoreslist').appendChild(li)
+     })
+     document.querySelector('.container').style.display = "none"
+     document.querySelector("#resultsDIV").style.display = "none";
+
+} 
+
